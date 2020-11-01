f2f1ee51480cefe2ae1f612b051075e5675fb287
Jason Schneider <Jason@Schneider-MacBook-Pro.local>
2020-11-01 01:42:00 -0400

    Continued with development on chart production.  Added time option mapping to chart formatting tuples into config to allow for more scalable changes to chart options.

--- backend/config.py
+++ backend/config.py
@@ -5,3 +5,22 @@ apikey = "RMSSTUCHMW9W04BBKRM7EMIP9OGFVYAE"
 infoAttrs = ["symbol", "description", "mark", "exchangeName", "assetType",
              "totalVolume", "divAmount", "divYield", "divDate", "netChange",
              "netPercentChangeInDouble"]
+
+# Defines the mapping of all chart time options (ex. "1y", "3m", "5d" meaning
+# "1 year", "3 month", and "5 day" history respectively) to appropriate
+# values for the API call as well as x axis unit format. Tuples are of format:
+# (english meaning for subtitle, periodType, period, frequencyType, frequency, 
+# canvas.js format string)
+chartMap = {
+    "1d": ("1 Day", "day", 1, "minute", 1, "h:mm"),
+    "3d": ("3 Days", "day", 3, "minute", 15, "DDD M/DD"),
+    "5d": ("5 Days", "day", 5, "minute", 30, "DDD M/DD"),
+    "10d": ("10 Days", "day", 10, "minute", 30, "M/DD"),
+    "1m": ("1 Month", "month", 1, "daily", 1, "M/DD"),
+    "3m": ("3 Months", "month", 3, "daily", 1, "M/DD"),
+    "6m": ("6 Months", "month", 6, "daily", 1, "M/DD"),
+    "1y": ("1 Year", "year", 1, "daily", 1, "M/YY"),
+    "3y": ("3 Years", "year", 3, "weekly", 1, "M/YY"),
+    "5y": ("5 Years", "year", 5, "weekly", 1, "M/YY"),
+    "YTD": ("Year to Date", "year", 1, "daily", 1, "M/DD")
+}
--- backend/stock_chart.py
+++ backend/stock_chart.py
@@ -7,40 +7,44 @@
 import requests as rq
 import config as cfg
 import pandas as pd
+import datetime as dt
 
 # This code based off of using canvas.js in front end later, do documentation
 # for this method after deciding.  Data will be formatted in a way to make
 # it easier for front end.
-def newGraph(sym, perType = "day", per = 10, hasExtHrs = True):
+def newGraph(sym, timeOption, hasExtHrs = True):
     # Will provide more robust settings options but currently implementing
     # basic functionality here
+    chartCfg = cfg.chartMap.get(timeOption)
+    texts = []
+    if "$" in sym:
+        texts[0] = sym + " Point History"
+        texts[1] = ""
+    else:
+        texts[0] = sym + " Price History"
+        texts[1] = "$"
     opts = {"animationEnabled": True,
             "theme": "light2",
             "exportEnabled": False,
             "title": {
-                text: r"{} Price History".format(sym)
+                "text": texts[0]
             },
             "subtitles": [{
-                text: r"Over last {} {}(s)".format(per, perType)
-            }]}
+                "text": "Over the Last " + chartCfg[0]
+            }]
+            "axisX": {
+                "valueFormatString": chartCfg[5],
+                "title": "Date"
+            }
+            "axisY": {
+                "prefix": texts[1],
+                "title": "Value"
+            }
+            "toolTip": {
+                content: "Date: {x}<br /><strong>Price:</strong><br />Open: "
+                         + "{y[0]}, Close: {y[3]}<br />High: {y[1]}, Low: "
+                         + "{y[2]}"
+            }
+            }
     data = None
     return (opts, data)
-
-# This function will likely not be needed if canvas.js approach is used
-def styleGraph(graph, useCandles, lineClr, ptType, ptClr, useShade, shadeClr,
-               showMinMax):
-    return graph
-
-# Take care of opts that are dependent on the period as well as the freqType
-# and freq to send to the payload
-def periodDependentSettings(perType, per):
-    # Switch that takes perType and decides what format x axis will follow
-    mapFreq = {
-
-    }
-    freqType =
-    freq = 
-    xAxisFormat = {
-        "day": "h:mm",
-        "month": 
-    }
