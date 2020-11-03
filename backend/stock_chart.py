a9c70af10f32b2138eb77688b5102ac5399645e1
Jason Schneider <Jason@Schneider-MacBook-Pro.local>
2020-11-03 00:42:33 -0500

    Basic preliminary backend implementation of stock_chart.py completed, however, cannot test functionality until frontend as the data is prepared for canvas.js and it needs to be tested there as it is visual

--- backend/stock_chart.py
+++ backend/stock_chart.py
@@ -12,18 +12,33 @@ import datetime as dt
 # This code based off of using canvas.js in front end later, do documentation
 # for this method after deciding.  Data will be formatted in a way to make
 # it easier for front end.
-def newGraph(sym, timeOption, hasExtHrs = True):
+def newGraph(sym, timeOption = "10d", hasExtHrs = True):
     # Will provide more robust settings options but currently implementing
     # basic functionality here
     chartCfg = cfg.chartMap.get(timeOption)
     texts = []
+
+    url = r"https://api.tdameritrade.com/v1/marketdata/{}/pricehistory".format(sym)
+    params = {"apikey": cfg.apikey,
+              "periodType": chartCfg[1],
+              "period": chartCfg[2],
+              "frequencyType": chartCfg[3],
+              "frequency": chartCfg[4],
+              "needExtendedHoursData": str(hasExtHrs).lower()}
+    candleList = rq.get(url, params).json().get("candles")
+
+    dataPoints = []
+    for candle in candleList:
+        x = candle["datetime"]
+        y = [candle["open"], candle["high"], candle["low"], candle["close"]]
+        dataPoints.append({"x": x, "y:": y})
+
     if "$" in sym:
-        texts[0] = sym + " Point History"
-        texts[1] = ""
+        texts.extend([sym + " Point History", "", "Points"])
     else:
-        texts[0] = sym + " Price History"
-        texts[1] = "$"
-    opts = {"animationEnabled": True,
+        texts.extend([sym + " Price History", "$", "Price"])
+
+    info = {"animationEnabled": True,
             "theme": "light2",
             "exportEnabled": False,
             "title": {
@@ -31,20 +46,20 @@ def newGraph(sym, timeOption, hasExtHrs = True):
             },
             "subtitles": [{
                 "text": "Over the Last " + chartCfg[0]
-            }]
+            }],
             "axisX": {
                 "valueFormatString": chartCfg[5],
                 "title": "Date"
-            }
+            },
             "axisY": {
                 "prefix": texts[1],
-                "title": "Value"
-            }
-            "toolTip": {
-                content: "Date: {x}<br /><strong>Price:</strong><br />Open: "
-                         + "{y[0]}, Close: {y[3]}<br />High: {y[1]}, Low: "
-                         + "{y[2]}"
-            }
-            }
-    data = None
-    return (opts, data)
+                "title": texts[2]
+            },
+            "data": [{
+                "type": "candlestick",
+                "xValueType": "dateTime",
+                "dataPoints": dataPoints
+            }]}
+    return info
+
+print(newGraph("TSLA"))
