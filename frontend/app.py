d7fe5def437a9f3587300bf929fc4624e159d67d
Jason Schneider <Jason@Schneider-MacBook-Pro.local>
2020-11-17 23:18:52 -0500

    Added additional skeleton html files for movers and options page (charts will be viewed along with info from the home page), and added navbar with basic customization for now.  Also added create_html.py to handle all API data html generation functionality.

--- frontend/app.py
+++ frontend/app.py
@@ -4,9 +4,17 @@ from backend import stock_info, stock_chart, stock_movers, stock_options
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def home():
     data = stock_movers.getMovers("$DJI", "up", "percent")
-    return render_template("index.html", data = data)
+    return render_template("home.html", data = data)
+
+@app.route("/options")
+def options():
+    return render_template("options.html")
+
+@app.route("/movers")
+def movers():
+    return render_template("movers.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
---
+++ frontend/create_html.py
@@ -0,0 +1,2 @@
+# File will have code that handles nearly all of the html formatting and
+# processing from backend API data to keep app.py as clutter-free as possible
