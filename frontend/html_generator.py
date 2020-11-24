8cdd95d904a0edbf97393ea4101636ff18cc10d3
Jason Schneider <Jason@Schneider-MacBook-Pro.local>
2020-11-24 00:59:21 -0500

    Homepage index at a glance card html generation with api data into intended format completed.  Added footer with buttons for potential functionality that will be implemented later.  In addition, began work on search form functionality

--- frontend/html_generator.py
+++ frontend/html_generator.py
@@ -7,7 +7,35 @@ from backend import stock_info as sinfo
 
 def htmlIndexCard(indexSym):
     data = sinfo.getBySymbol(indexSym, "indexCard")
-    html = "<h5>{}</h5><br>Change: {} ({}%)".format(data["lastPrice"],
-           data["netChange"], round(data["netPercentChangeInDouble"], 2))
+    lastPrice = round(data["lastPrice"], 2)
+    closePrice = round(data["closePrice"], 2)
+    netChange = round(data["netChange"], 2)
+    netPercentChangeInDouble = round(data["netPercentChangeInDouble"], 2)
+    if netChange < 0:
+        html = (
+        '<div class="row text-danger">'
+        '<div class="col"><b><u>Last:</u></b><br>'
+        '<span id="' + indexSym + 'lp">{}</span></div>'
+        '<div class="col"><b><u>Close:</u></b><br>'
+        '<span id="' + indexSym + 'cp">{}</span></div>'
+        '<div class="col"><b><u>Value Chg:</u></b><br>'
+        '<span id="' + indexSym + 'vchg">{}</span></div>'
+        '<div class="col"><b><u>% Chg:</u></b><br>'
+        '<span id="' + indexSym + 'pchg">{}%</div>'
+        '</div>'
+        ).format(lastPrice, closePrice, netChange, netPercentChangeInDouble)
+    else:
+        html = (
+        '<div class="row text-success">'
+        '<div class="col"><b><u>Last:</u></b><br>'
+        '<span id="' + indexSym + 'lp">{}</span></div>'
+        '<div class="col"><b><u>Close:</u></b><br>'
+        '<span id="' + indexSym + 'cp">{}</span></div>'
+        '<div class="col"><b><u>Value Chg:</u></b><br>'
+        '<span id="' + indexSym + 'vchg">{}</span></div>'
+        '<div class="col"><b><u>% Chg:</u></b><br>'
+        '<span id="' + indexSym + 'pchg">{}%</div>'
+        '</div>'
+        ).format(lastPrice, closePrice, netChange, netPercentChangeInDouble)
     return flask.Markup(html)
 #print(htmlIndexCard("$DJI"))
