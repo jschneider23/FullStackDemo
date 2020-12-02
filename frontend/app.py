a3d5aea10430a87ab338f9451cb79e564cc1c10b
Jason Schneider <Jason@Schneider-MacBook-Pro.local>
2020-12-02 01:47:40 -0500

    Modal appearance, routing, and opening after search form submission functionality has been implemented and will contain the stock 'reports' (quote and information). Currently contains dummy values and js/html for it will be cleaned up.  Will use similar modal functionality for options chain view

--- frontend/app.py
+++ frontend/app.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from backend import stock_info, stock_chart, stock_movers, stock_options
 import html_generator as hg
+import flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["POST", "GET"])
 def home():
-    context = {"DJI": hg.htmlIndexCard("$DJI"),
-               "SPXX": hg.htmlIndexCard("$SPX.X"),
-               "COMPX": hg.htmlIndexCard("$COMPX")}
+    errorMsg = None
+    context = {}
+    if request.method == "POST":
+        formInput = request.form["search"]
+        modalScript = flask.Markup("""
+            <script>
+                $('#viewStock').modal('show');
+                console.log($('#viewStock'))
+            </script>
+        """)
+        modalTitle = formInput + " Title"
+        modalBody = formInput + " Data would go here"
+        context.update({"modalScript": modalScript, "modalTitle": modalTitle,
+                        "modalBody": modalBody})
+    tupDJI = hg.htmlIndexCard("$DJI")
+    tupSPXX = hg.htmlIndexCard("$SPX.X")
+    tupCOMPX = hg.htmlIndexCard("$COMPX")
+    context.update({"clrDJI": tupDJI[0], "DJI": tupDJI[1], 
+                    "clrSPXX": tupSPXX[0], "SPXX": tupSPXX[1],
+                    "clrCOMPX": tupCOMPX[0], "COMPX": tupCOMPX[1]})
+    print("Context: " + str(context))
     return render_template("home.html", context = context)
 
 @app.route("/options")
--- frontend/html_generator.py
+++ frontend/html_generator.py
@@ -11,31 +11,42 @@ def htmlIndexCard(indexSym):
     closePrice = round(data["closePrice"], 2)
     netChange = round(data["netChange"], 2)
     netPercentChangeInDouble = round(data["netPercentChangeInDouble"], 2)
-    if netChange < 0:
-        html = (
-        '<div class="row text-danger">'
-        '<div class="col"><b><u>Last:</u></b><br>'
-        '<span id="' + indexSym + 'lp">{}</span></div>'
-        '<div class="col"><b><u>Close:</u></b><br>'
-        '<span id="' + indexSym + 'cp">{}</span></div>'
-        '<div class="col"><b><u>Value Chg:</u></b><br>'
-        '<span id="' + indexSym + 'vchg">{}</span></div>'
-        '<div class="col"><b><u>% Chg:</u></b><br>'
-        '<span id="' + indexSym + 'pchg">{}%</div>'
-        '</div>'
-        ).format(lastPrice, closePrice, netChange, netPercentChangeInDouble)
-    else:
-        html = (
-        '<div class="row text-success">'
-        '<div class="col"><b><u>Last:</u></b><br>'
-        '<span id="' + indexSym + 'lp">{}</span></div>'
-        '<div class="col"><b><u>Close:</u></b><br>'
-        '<span id="' + indexSym + 'cp">{}</span></div>'
-        '<div class="col"><b><u>Value Chg:</u></b><br>'
-        '<span id="' + indexSym + 'vchg">{}</span></div>'
-        '<div class="col"><b><u>% Chg:</u></b><br>'
-        '<span id="' + indexSym + 'pchg">{}%</div>'
-        '</div>'
-        ).format(lastPrice, closePrice, netChange, netPercentChangeInDouble)
+    cardColor = "bg-danger" if netChange < 0.0 else "bg-success"
+    html = """
+        <div class="row">
+        <div class="col"><b><u>Last:</u></b><br>
+        <span id=" + indexSym + lp">{}</span></div>
+        <div class="col"><b><u>Close:</u></b><br>
+        <span id=" + indexSym + cp">{}</span></div>
+        <div class="col"><b><u>Value Chg:</u></b><br>
+        <span id=" + indexSym + vchg">{}</span></div>
+        <div class="col"><b><u>% Chg:</u></b><br>
+        <span id=" + indexSym + pchg">{}%</div>
+        </div>
+        """.format(lastPrice, closePrice, netChange, netPercentChangeInDouble)
+    return (cardColor, flask.Markup(html))
+
+def htmlModal(sym):
+    html = """
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">{} Title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                {} Information
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+    """.format(sym, sym)
     return flask.Markup(html)
 #print(htmlIndexCard("$DJI"))
